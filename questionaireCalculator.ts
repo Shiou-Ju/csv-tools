64ec64fe05406b39cd7125a5963e3fa4370755aa
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2022-05-24 21:54:50 +0800

    feat: implement read file

---
+++ questionaireCalculator.ts
@@ -0,0 +1,10 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as csv from 'fast-csv';
+
+fs.createReadStream(path.resolve(__dirname, 'csv', 'attachment.csv'))
+    .pipe(csv.parse({ headers: true }))
+    .on('error', error => console.error(error))
+    .on('data', row => console.log(row))
+    .on('end', (rowCount: number) => console.log(`Parsed ${rowCount} rows`));
+
